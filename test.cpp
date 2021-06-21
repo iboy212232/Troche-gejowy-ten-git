8cec334c3c8ed03b9105e0a17015a062a490e8d0
iboy <56487051+iboy212232@users.noreply.github.com>
2021-06-21 19:23:48 +0200

    test.cpp
    testowa baza

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <dos.h>
+#include <windows.h>
+using namespace std;
+int main()
+{
+  cout<< "Hello"<<endl;
+  cout<<"You are me test subject."<<endl;
+  sleeep(3);
+  system("CLS");
+  
+  
+  
+  
+  
+  
+ return 0; 
+}
